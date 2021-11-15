49f7d976f569b74add5c7ea7a6e9b2b696116ed6
Clarke Lee <68850705+Clarkedlee@users.noreply.github.com>
2021-11-15 16:31:33 +1300

    Update settings.py
    Delete Secrets

--- Cockpit_web/settings.py
+++ Cockpit_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$@)j_lhn9)!b!^i8c1o$2fo8+6auhs(htioc6cw&v&0v*gn)#='
+# SECRET_KEY = '*********'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
